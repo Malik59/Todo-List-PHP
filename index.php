2cabe848caecdaa396adb155f6556eec9594a476
Malik59 <mal.k@laposte.net>
2024-01-27 08:21:39 +0100

    premier commit: création header footer et content

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@400;700&family=Merienda:wght@700&display=swap" rel="stylesheet">    
+    <link rel="stylesheet" href="public/css/style.css">
+    <title>Todo list</title>
+</head>
+<body>
+    <div class="container">
+        <header>
+            <div class="logo">My todo list</div>
+        </header>
+
+        <div class="content">
+            <div class="todo-container">
+                <h1>Ma Liste</h1>
+                <div class="todo-form">
+
+                </div>
+                <div class="todo-list">
+
+                </div>
+            </div>
+        </div>
+
+        <footer> Copyright 2024 </footer>
+    </div>
+
+</body>
+</html>
